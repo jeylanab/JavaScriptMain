b525694489db8e7ea8687a5932f6d0a32c42e170
jeyabdo <joephoenix755@gmail.com>
2023-12-28 06:46:15 +0000

    switch statement

--- Conditional Statement/checkbox and radio button/index.js
+++ Conditional Statement/checkbox and radio button/index.js
@@ -1,12 +1,39 @@
+// if else statement with radio and checkbox input type...
 
-document.getElementById("myButton").onclick = function () {
- const MyCheckBox = document.getElementById("MyCheckBox");
+
+// document.getElementById("myButton").onclick = function () {
+//  const MyCheckBox = document.getElementById("MyCheckBox");
  
-if (document.getElementById("MyCheckBox").checked) {
-    console.log( "the box is checked ")
-}
-else {
-    console.log ("the bos has not checked");
-}
+// if (document.getElementById("MyCheckBox").checked) {
+//     console.log( "the box is checked ")
+// }
+// else {
+//     console.log ("the bos has not checked");
+// }
 
+// }
+
+
+
+// SWITCH STATEMENT 
+// is used to switch between conditions 
+
+let grade;
+grade = prompt("enter grade");
+
+switch(true) {
+    case grade >= 90:
+        console.log("EXCELLENT!");
+        break;
+    case grade >=80:
+        console.log ("VERY GOOD");
+        break;
+    case grade >=70:
+        console.log ("GOOD");
+        break;
+    case grade >=60:
+        console.log ("barely GOOD");
+         break;
+    default :
+        console.log("Invalid");
 }
