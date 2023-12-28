e42ed43c891a4c395893a1e6d0f8d4dad69e08b9
jeyabdo <joephoenix755@gmail.com>
2023-12-28 06:27:27 +0000

    added input button with jS dom

---
+++ Conditional Statement/checkbox and radio button/index.js
@@ -0,0 +1,12 @@
+
+document.getElementById("myButton").onclick = function () {
+ const MyCheckBox = document.getElementById("MyCheckBox");
+ 
+if (document.getElementById("MyCheckBox").checked) {
+    console.log( "the box is checked ")
+}
+else {
+    console.log ("the bos has not checked");
+}
+
+}
