ceb065ee2ad39ba849f7488283d17916672f5952
jeyabdo <joephoenix755@gmail.com>
2023-12-28 05:40:10 +0000

    conditional statement code added

---
+++ Conditional Statement/index.js
@@ -0,0 +1,22 @@
+// Conditional statement practice
+console.log("My name is Jeylan");
+
+let number = 20;
+
+if(number >= 40){
+    console.log("The number is Greater ");
+}
+else {
+    console.log("The number is lowwer than 40");
+}
+
+// use boolean 
+
+let Online = false;
+ if (Online) {
+    console.log("You are Online!");
+
+ }
+else {
+    console.log("You are Offline!");
+}
