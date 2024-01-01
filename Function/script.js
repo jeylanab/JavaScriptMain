c86cc0e1721966cb4414c0cdb45c3ab080c23695
jeyabdo <joephoenix755@gmail.com>
2024-01-01 09:20:58 +0000

    added funtion code

---
+++ Function/script.js
@@ -0,0 +1,12 @@
+// THIS ARE FUNTION
+// function is refer as a small peice of insruction that can be invoked many times
+
+function MyFunction(){
+    console.log("Happy birth day to you , happy birthday happy birth day to you");
+}
+ 
+MyFunction();
+MyFunction();
+MyFunction();
+MyFunction();
+MyFunction();
