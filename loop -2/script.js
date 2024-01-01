f5ddb12051f7d5e932d60c6458d795fd3aebb7b6
jeyabdo <joephoenix755@gmail.com>
2024-01-01 09:04:52 +0000

    added loop example

---
+++ loop -2/script.js
@@ -0,0 +1,11 @@
+console.log("this is my first work");
+
+
+for (let i = 0; i <= 3; i++){
+    for ( let j = 0; j <= 3; j++){
+        console.log(" j = {j}");
+        
+
+    }
+  console.log();
+}
