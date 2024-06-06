0e41fa74ca055d4e14c41b5654b3e03bc5e019e9
jeylanab <jeylantesi@gmail.com>
2024-06-06 12:14:56 +0530

    added express file

--- AFTER BOOTCAMP/Node/express/server.js
+++ AFTER BOOTCAMP/Node/express/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("hello this is  my first server code")
     
 })
+app.get('/api/user', (req, res) => {
+    res.send(
+        {id: 1, username: 'jeylan' , age: 23},
+        {id: 2, username: 'jack' , age: 24},
+        { id: 3, username: 'james', age: 25 },
+        {id: 4, username: "jidion" , age :55}
+    )
+})
+app.get('/api/about', (req, res) => {
 
-app.listen(8000, () => {
-    console.log('you server is running on the http://localhost:8000')
+    res.send(
+       "<div>HELLO THIS JEYLAN IF YOU HAVE SOME QUESTION DON'T HESITATE TO CONTACT ME AGAIN I AM ALWAYS AVAILABLE</div>" 
+    )
+    
+})
+
+app.listen(port, () => {
+    console.log(`the the server is running on ${port}`)
 })
